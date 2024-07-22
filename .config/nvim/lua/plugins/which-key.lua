6e47db710fb0e1ef50a93226baa150a64750f948
Ethan Anthony <ethan.anthony@du.edu>
2024-07-21 19:51:24 -0600

    Disabled which-key temporarily

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,44 +1,37 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
-  },
-  config = function()
-    require("which-key").setup({
-      preset = "modern",
-      delay = 500,
-      modes = {
-        n = true, -- Normal mode
-        i = false, -- Insert mode
-        x = false, -- Visual mode
-        s = false, -- Select mode
-        o = false, -- Operator pending mode
-        t = false, -- Terminal mode
-        c = false, -- Command mode
-      },
-      icons = {
-        rules = false
-      }
-    })
-    local wk = require("which-key")
-    wk.add({
-      { "<leader>f", group = "File Management" },
-      { "<leader>g", group = "Git Management" },
-      { "<leader>v", group = "View Management" },
-      { "<leader>",  group = "Leader Keybinds" },
-      { "g",         group = "Go To" },
-      { "z",         group = "Folds" },
-      { "'",         group = "Marks" },
-      { "`",         group = "Marks" },
-      { "\"",         group = "Registers" },
-      { "<c-w>",     group = "Window Management" },
-      { "[",         group = "Backward" },
-      { "]",         group = "Forward" },
-    })
-  end
+  -- "folke/which-key.nvim",
+  -- event = "VeryLazy",
+  -- config = function()
+  --   require("which-key").setup({
+  --     preset = "modern",
+  --     delay = 500,
+  --     modes = {
+  --       n = true, -- Normal mode
+  --       i = false, -- Insert mode
+  --       x = false, -- Visual mode
+  --       s = false, -- Select mode
+  --       o = false, -- Operator pending mode
+  --       t = false, -- Terminal mode
+  --       c = false, -- Command mode
+  --     },
+  --     icons = {
+  --       rules = false
+  --     }
+  --   })
+  --   local wk = require("which-key")
+  --   wk.add({
+  --     { "<leader>f", group = "File Management" },
+  --     { "<leader>g", group = "Git Management" },
+  --     { "<leader>v", group = "View Management" },
+  --     { "<leader>",  group = "Leader Keybinds" },
+  --     { "g",         group = "Go To" },
+  --     { "z",         group = "Folds" },
+  --     { "'",         group = "Marks" },
+  --     { "`",         group = "Marks" },
+  --     { "\"",         group = "Registers" },
+  --     { "<c-w>",     group = "Window Management" },
+  --     { "[",         group = "Backward" },
+  --     { "]",         group = "Forward" },
+  --   })
+  -- end
 }
