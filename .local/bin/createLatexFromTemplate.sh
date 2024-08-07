8a5d091f742c5d83c245fd1bcd0194ebcef08055
Ethan Anthony <ethan.anthony@du.edu>
2024-08-07 15:33:35 -0600

    added help messages

--- .local/bin/createLatexFromTemplate.sh
+++ .local/bin/createLatexFromTemplate.sh
@@ -1,59 +1,95 @@
 #!/usr/bin/env bash
 
-while getopts 'l:f:' opt; do
+echoHelpMessage () {
+  echo "createLatexFromTemplate - create new files in the current course directory"
+  echo "                          based on templates stored in the appropriate xlatex"
+  echo "                          directory."
+  echo
+  echo "Use templates stored in \$CURRQUARTER/xlatex/templates/ to create new files in"
+  echo "their approriate location within the current course directory and sequentially"
+  echo "name them according to which files already exist."
+  echo
+  echo "Available Options:"
+  echo "-l [FILE PATH]        Creates a new lecture file in currentCourse/notes/ named"
+  echo "                      lec_XXX.tex, where XXX is the next lowest number currently"
+  echo "                      unused."
+  echo "-f [FILE PATH]        Creates a new figure file in currentCourse/notes/figures/"
+  echo "                      named fig_XXX.tex, where XXX is the next lowest number"
+  echo "                      currently unused."
+}
+
+createFile () {
+  templatePath="$1"
+  templateType="$2"
+  if [[ "$templateType" = "Figure" ]]; then
+    fileBase=fig_
+    workingDirectory=$CURRCOURSE/notes/figures
+  elif [[ "$templateType" = "Lecture" ]]; then
+    fileBase=lec_
+    workingDirectory=$CURRCOURSE/notes
+  fi
+
+  mapfile -t files < <(find $workingDirectory -mindepth 1 -maxdepth 1 -type f)
+  for i in ${!files[@]}; do
+    filename=$(basename "${files[$i]}")
+    if [[ ! "$filename" =~ ^$fileBase(0*)([0-9]*)(_[0-9])?\.tex$ ]]; then
+      files=( "${files[@]:0:$i}" "${files[@]:(($i + 1))}" )
+    fi
+  done
+
+  if [[ ${#files[@]} -gt 0 ]]; then
+    lastFile=$(basename ${files[${#files[@]} - 1]})
+    if [[ "$lastFile" =~ ^$fileBase(0*)([0-9]*)(_[0-9])?\.tex$ ]]; then
+      number="${BASH_REMATCH[2]}"
+      ((number++))
+      number=$(printf "%03d" $number)
+
+      newFilePath="$workingDirectory/$fileBase$number.tex"
+    else
+      echo "No match"
+    fi
+  else
+    number=001
+    newFilePath="$workingDirectory/$fileBase$number.tex"
+  fi
+
+  cp "$templatePath" "$newFilePath"
+
+  if [[ -n $(find "$workingDirectory" "$newFilePath") ]]; then
+    line=$(cat $templatePath | rg \title)
+    title=$(echo $line | sed 's/\\title{\(.*\)}/\1/')
+
+    notify-send "Created New $templateType" "$title
+$(basename $newFilePath) in $(getCurrentCourseInfo -s) - $(getCurrentCourseInfo -t)
+"
+  else
+    notify-send "Did Not Create New $templateType" "$newFilePath"
+  fi
+}
+
+while getopts 'l:f:h' opt; do
   case "$opt" in
     l)
-      workingDirectory=$HOME/Documents/School/currentCourse/notes
-      fileBase="lec_"
-      template="$OPTARG"
-      templateType="Lecture"
+      tp="$OPTARG"
+      tt="Lecture"
+      createFile "$tp" "$tt"
       ;;
 
     f)
-      workingDirectory=$HOME/Documents/School/currentCourse/notes/figures
-      fileBase="fig_"
-      template="$OPTARG"
-      templateType="Figure"
+      tp="$OPTARG"
+      tt="Figure"
+      createFile "$tp" "$tt"
       ;;
 
-    ?|h)
+    h)
+      echoHelpMessage
+      ;;
+
+    ?)
       echo "Usage: $(basename $0) [-l arg] [-f arg]"
+      echo
+      echoHelpMessage
       exit 1
       ;;
   esac
 done
-shift "$(($OPTIND -1))"
-
-mapfile -t files < <(find $workingDirectory -mindepth 1 -maxdepth 1 -type f)
-
-for i in ${!files[@]}; do
-  filename=$(basename "${files[$i]}")
-  if [[ ! "$filename" =~ ^$fileBase(0*)([0-9]*)(_[0-9])?\.tex$ ]]; then
-    files=( "${files[@]:0:$i}" "${files[@]:(($i + 1))}" )
-  fi
-done
-
-if [[ ${#files[@]} -gt 0 ]]; then
-  lastFile=$(basename ${files[${#files[@]} - 1]})
-  if [[ "$lastFile" =~ ^$fileBase(0*)([0-9]*)(_[0-9])?\.tex$ ]]; then
-    number="${BASH_REMATCH[2]}"
-    ((number++))
-    number=$(printf "%03d" $number)
-
-    newFilePath="$workingDirectory/$fileBase$number.tex"
-  else
-    echo "No match"
-  fi
-else
-  number=001
-  newFilePath="$workingDirectory/$fileBase$number.tex"
-fi
-
-cp "$template" "$newFilePath"
-
-line=$(cat $template | rg \title)
-title=$(echo $line | sed 's/\\title{\(.*\)}/\1/')
-
-notify-send "Created New $templateType" "$title
-$(basename $newFilePath) in $(getCurrentCourseInfo.sh -s) - $(getCurrentCourseInfo.sh -t)
-"
--- .local/bin/setCurrentCourse.sh
+++ .local/bin/setCurrentCourse.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-quarterDirectory=$HOME/Documents/School/quarter04
-currentCourseDirectory=$HOME/Documents/School/currentCourse
+silent=false
 
-getNewCourseDirectory() {
+getNewCourseDirectory () {
   time=$(date +"%H%M")
   time=$(echo $time | sed 's/^0*//')
   day=$(date +"%a")
 
-  paths=$(find $quarterDirectory -maxdepth 1 -type d)
+  paths=$(find $CURRCOURSE -mindepth 1 -maxdepth 1 -type d)
   coursePaths=()
   for path in $paths; do
-    if [[ $path != $quarterDirectory && ! $path =~ "xlatex" ]]; then
+    if [[ ! $path =~ "xlatex" ]]; then
       coursePaths+=($path);
     fi
   done
@@ -38,40 +37,91 @@ getNewCourseDirectory() {
     fi
   done
 
-
   echo -1
   return 0
 }
 
-setCurrentCourse() {
-  symLinkTarget=$1
-  symLinkDirectory=$currentCourseDirectory
+setCurrentCourse () {
+  symLinkTarget="$1"
+  symLinkDirectory=$CURRCOURSE
   ln -sfn "$symLinkTarget" "$symLinkDirectory"
-  return 0
 }
 
-getCurrentCourse () {
-  directory=$(readlink -f $currentCourseDirectory)
-  code=$(cat "$directory/info.json" | jq -r .code)
-  number=$(cat "$directory/info.json" | jq -r .number)
-  title="$(cat "$directory/info.json" | jq -r .title)"
-  echo "$code $number - $title"
-  return 0
+echoHelpMessage () {
+  echo "setCurrentCourse - a tool to manage the current course"
+  echo
+  echo "Change the current course value from the command line and notify the user of"
+  echo "what the current course is. Uses freedesktop.org notification standards"
+  echo
+  echo "Usage: setCurrentCourse [-q] [-g] [-a] [-s DIRECTORY]"
+  echo "  Currently, no options are mutually exclusive. However, using both -a and -s"
+  echo "  in the same run is redundant and discouraged."
+  echo
+  echo "Available Options:"
+  echo "  -q                  Prevents sending system notifications from this program"
+  echo "  -g                  Send a notification containing the information of the"
+  echo "                      current course."
+  echo "  -a                  Read course information from info.json files in course"
+  echo "                      directories and automatically update current course if"
+  echo "                      applicable"
+  echo "  -s DIRECTORY        Sets the current course to DIRECTORY. DIRECTORY must be"
+  echo "                      a child of \$CURRCOURSE environment variable."
 }
 
-if [[ $# = 0 ]]; then
-  newCourseDirectory=$(getNewCourseDirectory)
-  if [[ -d "$newCourseDirectory" ]]; then
-    setCurrentCourse $newCourseDirectory
-    notify-send "Current Course Changed" "$(getCurrentCourse)"
-  else
-    notify-send "Current Course Unchanged" "$(getCurrentCourse)"
-  fi
-else
-  if [ -d  $1 ]; then # TODO: better path verification
-    setCurrentCourse $1
-    notify-send "Current Course Changed" "$(getCurrentCourse)"
-  else
-    notify-send "Current Course Unchanged" "$(getCurrentCourse)"
-  fi
-fi
+while getopts 'qgas:h' opt; do
+  case "$opt" in
+    q)
+      silent=true
+      ;;
+
+    g)
+      title=$(getCurrentCourseInfo -t)
+      short=$(getCurrentCourseInfo -s)
+      if [[ "$silent" = "false" ]]; then
+        notify-send "Get Current Course" "$short - $title"
+      fi
+      ;;
+
+    a)
+      newCourseDirectory=$(getNewCourseDirectory)
+      if [ -d "$newCourseDirectory" ]; then
+        setCurrentCourse "$newCourseDirectory"
+        title=$(getCurrentCourseInfo -t)
+        short=$(getCurrentCourseInfo -s)
+        if [[ "$silent" = "false" ]]; then
+          notify-send "Current Course Changed" "$short - $title"
+        fi
+      else
+        title=$(getCurrentCourseInfo -t)
+        short=$(getCurrentCourseInfo -s)
+        if [[ "$silent" = "false" ]]; then
+          notify-send "Current Course Unchanged" "$short - $title"
+        fi
+      fi
+      ;;
+
+    s)
+      newCourseDirectory="$(pwd)/$OPTARG"
+      result=$(find "$CURRQUARTER" -type d -wholename "$newCourseDirectory")
+      if [[ -n $result ]]; then
+        setCurrentCourse "$newCourseDirectory"
+        title=$(getCurrentCourseInfo -t)
+        short=$(getCurrentCourseInfo -s)
+        if [[ "$silent" = "false" ]]; then
+          notify-send "Current Course Changed" "$short - $title"
+        fi
+      else
+        title=$(getCurrentCourseInfo -t)
+        short=$(getCurrentCourseInfo -s)
+        if [[ "$silent" = "false" ]]; then
+          notify-send "Current Course Unchanged" "$short - $title"
+        fi
+      fi
+      ;;
+
+    h)
+      echoHelpMessage
+      ;;
+
+    esac
+  done
