7a53879eb0f35dc9038c5f14ebcb5398bb51e744
Ethan Anthony <ethan.anthony@du.edu>
2024-08-09 00:00:47 -0600

    added script for rofi to open important things relating to a course

--- .config/rofi/scripts/rofiCourseBookmarks.sh
+++ .config/rofi/scripts/rofiCourseBookmarks.sh
@@ -3,15 +3,35 @@
 mapfile -t messages< <(cat $CURRCOURSE/info.json | jq -r '.bookmarks | keys[]')
 mapfile -t addresses< <(cat $CURRCOURSE/info.json | jq -r '.bookmarks[]')
 
+commands=()
+for address in ${addresses[@]}; do
+  commands+=("librewolf $address")
+done
+
+messages+=("Open in Terminal")
+commands+=("foot -D $CURRCOURSE")
+
+messages+=("Open in File Browser")
+commands+=("foot -e yazi $CURRCOURSE")
+
+messages+=("Edit Last Lecture")
+commands+=("foot -e nvim $(getCourseInfo --last-lecture)")
+
+messages+=("View Course Notes (Compile)")
+commands+=("cd $CURRCOURSE/notes && pdflatex ./master.tex && zathura ./master.pdf")
+
+messages+=("View Course Notes (Don't Compile)")
+commands+=("zathura $CURRCOURSE/notes/master.pdf")
+
 # Rofi Logic
 if [[ $# = 0 ]]; then
-  for message in ${messages[@]}; do
-    echo "$message"
+  for i in ${!messages[@]}; do
+    echo "${messages[$i]}"
   done
 else
   for i in ${!messages[@]}; do
     if [[ $1 = "${messages[$i]}" ]]; then
-      librewolf "${addresses[$i]}"
+      coproc ( bash -c "${commands[$i]}"  > /dev/null  2>&1 )
     fi
   done
 fi
