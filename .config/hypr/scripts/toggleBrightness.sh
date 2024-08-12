b3ba8de62f9df594a07af4c8d8044195f3f20a27
Ethan Anthony <ethan.anthony@du.edu>
2024-08-12 16:31:09 -0600

    removed comments

--- .config/hypr/scripts/toggleBrightness.sh
+++ .config/hypr/scripts/toggleBrightness.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# Get the current brightness level using brightnessctl
 current_brightness=$(brightnessctl -m | grep -oP '\d+(?=%)')
 
-# Check if current brightness is over 50%
 if [ "$current_brightness" -gt 50 ]; then
-    # Set brightness to 0%
     brightnessctl set 0%
-    echo "Brightness set to 0%."
 else
-    # Set brightness to 100%
     brightnessctl set 100%
-    echo "Brightness set to 100%."
 fi
