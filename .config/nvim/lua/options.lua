051a311028e81a50c53f5f0f1113d720a1341a8f
Ethan Anthony <ethan.anthony@du.edu>
2024-08-20 19:04:27 -0600

    added system cliobard integration

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -17,3 +17,6 @@ vim.cmd("set foldcolumn=1")
 vim.cmd("set foldlevel=20")
 vim.cmd("set foldmethod=expr")
 vim.cmd("set foldexpr=nvim_treesitter#foldexpr()")
+
+-- CLIPBOARD INTEGRATION
+vim.cmd("set clipboard=unnamedplus")
