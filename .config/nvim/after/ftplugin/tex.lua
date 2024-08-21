18f411d6d308cca73193ecc524d0ca1024fed75b
Ethan Anthony <ethan.anthony@du.edu>
2024-08-20 19:04:15 -0600

    tried and failed to add --shell-escape for vimtex options

--- .config/nvim/after/ftplugin/tex.lua
+++ .config/nvim/after/ftplugin/tex.lua
@@ -1,1 +1,15 @@
--- vim.keymap.del("i", "]]")
+vim.g.vimtex_compiler_latexmk = {
+  aux_dir = '',
+  out_dir = '',
+  callback = 1,
+  continuous = 1,
+  executable = 'latexmk',
+  hooks = {},
+  options = {
+    '-verbose',
+    '-file-line-error',
+    '-synctex=1',
+    '-interaction=nonstopmode',
+    '--shell-escape',
+  },
+}
