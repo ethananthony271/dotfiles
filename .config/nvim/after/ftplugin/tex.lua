54126f67fd358859d0fbf70f2c91a21dffa6b3e8
Ethan Anthony <ethan.anthony@du.edu>
2024-08-25 22:19:00 -0400

    Added spell-check

--- .config/nvim/after/ftplugin/tex.lua
+++ .config/nvim/after/ftplugin/tex.lua
@@ -1,15 +1,17 @@
-vim.g.vimtex_compiler_latexmk = {
-  aux_dir = '',
-  out_dir = '',
-  callback = 1,
-  continuous = 1,
-  executable = 'latexmk',
-  hooks = {},
-  options = {
-    '-verbose',
-    '-file-line-error',
-    '-synctex=1',
-    '-interaction=nonstopmode',
-    '--shell-escape',
-  },
-}
+vim.cmd("set spell")
+
+-- vim.g.vimtex_compiler_latexmk = {
+--   aux_dir = '',
+--   out_dir = '',
+--   callback = 1,
+--   continuous = 1,
+--   executable = 'latexmk',
+--   hooks = {},
+--   options = {
+--     '-verbose',
+--     '-file-line-error',
+--     '-synctex=1',
+--     '-interaction=nonstopmode',
+--     '--shell-escape',
+--   },
+-- }
