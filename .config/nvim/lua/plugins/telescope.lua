192b6712f85172c8aef4026aa1870acaa02ca88a
Ethan Anthony <ethan.anthony@du.edu>
2024-08-26 18:32:38 -0400

    added keybinds to telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -8,6 +8,20 @@ return {
     },
     config = function()
       local builtin = require("telescope.builtin")
+      require('telescope').setup{
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-k>"] = "move_selection_previous",
+              ["<C-j>"] = "move_selection_next",
+              ["<C-q>"] = "close",
+              ["<C-m>"] = "select_vertical",
+              ["<C-n>"] = "select_horizontal",
+              ["<C-t>"] = "select_tab",
+            }
+          }
+        }
+      }
     end
   },
 }
