bb36743dc7cc4077acc2ad9dc05f2cf34b0ccca6
Ethan Anthony <ethan.anthony@du.edu>
2024-08-29 20:17:49 -0600

    removed tab getting and made windows reflect size change

--- .config/rofi/scripts/rofiAddBookmark.sh
+++ .config/rofi/scripts/rofiAddBookmark.sh
@@ -1,42 +1,47 @@
 #!/usr/bin/env bash
 
 rofiConfig="-config ~/.config/rofi/abokMenu.rasi"
-buffer=$(lz4jsoncat ~/.librewolf/q11askuj.default-default/sessionstore-backups/recovery.jsonlz4)
+# buffer=$(lz4jsoncat ~/.librewolf/q11askuj.default-default/sessionstore-backups/recovery.jsonlz4)
+#
+# declare -a messages
+#
+# numberOfWindows=$(echo $buffer | jq '.windows | length')
+# for nWindow in $(seq 0 $(( $numberOfWindows - 1 ))); do
+#
+#   windowTabs=($(echo $buffer | jq --arg nWindow $nWindow ".windows[$nWindow] | .tabs[] | .entries | length"))
+#
+#   for nTab in $(seq 0 $(( ${#windowTabs[@]} - 1))); do
+#     tabVersion=$(( ${windowTabs[$nTab]} - 1 ))
+#     url=$(echo $buffer | jq --arg nWindow $nWindow -r --arg nTab $nTab --arg tabVersion $tabVersion ".windows[$nWindow] | .tabs[$nTab] | .entries[$tabVersion] | .url" | sed 's/&/\&amp;/g')
+#     title=$(echo $buffer | jq --arg nWindow $nWindow -r --arg nTab $nTab --arg tabVersion $tabVersion ".windows[$nWindow] | .tabs[$nTab] | .entries[$tabVersion] | .title" | sed 's/&/\&amp;/g')
+#     messages+=("$title <span weight='light' style='italic'>($url)</span>")
+#   done
+# done
+#
+# echoMessages () {
+#   BIFS="$IFS"
+#   IFS=$'\n'
+#   for message in ${messages[@]}; do
+#     echo -e "$message"
+#   done
+#   IFS="$BIFS"
+# }
 
-declare -a messages
+# title=$(echoMessages | rofi $rofiConfig -markup-rows -dmenu -p "bookmark title" | sed '/<span/ s/\(.*\)\s\s*<span.*>/\1/')
+title=$(rofi $rofiConfig -markup-rows -dmenu -theme-str 'mainbox {children: [ "inputbar" ];} window {height: 44px; x-offset: 100px;} inputbar {spacing: 0px;} entry {placeholder: "bookmark title";}' -p "")
 
-numberOfWindows=$(echo $buffer | jq '.windows | length')
-for nWindow in $(seq 0 $(( $numberOfWindows - 1 ))); do
-
-  windowTabs=($(echo $buffer | jq --arg nWindow $nWindow ".windows[$nWindow] | .tabs[] | .entries | length"))
-
-  for nTab in $(seq 0 $(( ${#windowTabs[@]} - 1))); do
-    tabVersion=$(( ${windowTabs[$nTab]} - 1 ))
-    url=$(echo $buffer | jq --arg nWindow $nWindow -r --arg nTab $nTab --arg tabVersion $tabVersion ".windows[$nWindow] | .tabs[$nTab] | .entries[$tabVersion] | .url" | sed 's/&/\&amp;/g')
-    title=$(echo $buffer | jq --arg nWindow $nWindow -r --arg nTab $nTab --arg tabVersion $tabVersion ".windows[$nWindow] | .tabs[$nTab] | .entries[$tabVersion] | .title" | sed 's/&/\&amp;/g')
-    messages+=("$title <span weight='light' style='italic'>($url)</span>")
-  done
-done
-
-echoMessages () {
-  BIFS="$IFS"
-  IFS=$'\n'
-  for message in ${messages[@]}; do
-    echo -e "$message"
-  done
-  IFS="$BIFS"
-}
-
-title=$(echoMessages | rofi $rofiConfig -markup-rows -dmenu -p "bookmark title" | sed '/<span/ s/\(.*\)\s\s*<span.*>/\1/')
 if [[ -z "$title" ]]; then
   notify-send "No Bookmark Created" "Title was not specified"
   exit 1
 fi
-url=$(echoMessages | rofi $rofiConfig -dmenu -markup-rows -p "bookmark url" | sed '/<span/ s/.*<span.*>(\(.*\))<\/span>/\1/')
+
+# url=$(echoMessages | rofi $rofiConfig -dmenu -markup-rows -p "bookmark url" | sed '/<span/ s/.*<span.*>(\(.*\))<\/span>/\1/')
+url=$(rofi $rofiConfig -markup-rows -dmenu -theme-str 'mainbox {children: [ "inputbar" ];} window {height: 44px;} inputbar {spacing: 0px;} entry {placeholder: "bookmark url";}' -p "")
 if [[ -z "$url" ]]; then
   notify-send "No Bookmark Created" "URL was not specified"
   exit 1
 fi
+
 category=$(jq --sort-keys -r "keys[]" $BOOKMARKS | rofi $rofiConfig -dmenu -p "bookmark category")
 if [[ -z "$category" ]]; then
   notify-send "No Bookmark Created" "Category was not specified"
